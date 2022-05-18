f42ff0abfb7e0add92982ff92ae06567d8f8bd9e
Juanma Cano <juanmacano@gmail.com>
2022-05-18 16:17:21 +0200

    <svelte:head>

--- src/routes/acerca.svelte
+++ src/routes/acerca.svelte
@@ -1,1 +1,4 @@
+<svelte:head>
+	<title>Acerca de</title>
+</svelte:head>
 <h1 class="my-9 py-9 text-5xl">Página de acerca de</h1>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,7 @@
+<svelte:head>
+	<title>Introducción a SvelteKit</title>
+</svelte:head>
+
 <center class="mt-9">
 	<img src="netflix.svg" alt="" />
 	<h1 class="bold text-5xl">USERS API</h1>
